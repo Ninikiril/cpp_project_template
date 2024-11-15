a47dbc15ea9b05cb88f7778d60f7f0f36c4ac581
Ninikiril <nicolas.planard.luong@gmail.com>
2024-11-15 15:24:57 +0100

    add boost and catch2

--- include/lib/lib.hpp
+++ include/lib/lib.hpp
@@ -1,13 +1,9 @@
 #pragma once
 
 /**
- * @brief Computes the inverse square root of a given number.
- * 
- * This function uses a fast approximation algorithm to compute the inverse square root
- * of a floating-point number. It is commonly used in graphics and game development
- * for its performance benefits.
- * 
- * @param number The floating-point number for which to compute the inverse square root.
- * @return The inverse square root of the given number.
+ * @name f_example
+ * @brief Example function
+ * @param a
+ * @return true if a is greater than 0
  */
-float Q_rsqrt( float number );
+bool f_example(float a);
