6adcbe9a8780b0b5c88b37774dffae1ec1dffc1e
Ninikiril <nicolas.planard.luong@gmail.com>
2024-11-15 23:57:34 +0100

    adding boost, update template and tried adding sonar but failed

--- include/lib/lib.hpp
+++ include/lib/lib.hpp
@@ -1,9 +1,12 @@
 #pragma once
 
 /**
- * @name f_example
  * @brief Example function
- * @param a
- * @return true if a is greater than 0
+ * 
+ * @details This function is an example of a function. It returns true if a is greater than 0, false otherwise.
+ * 
+ * @param a 
+ * @return true 
+ * @return false 
  */
 bool f_example(float a);
